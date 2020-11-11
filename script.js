4980ec9537b0abaf533548210e3df974522a228a
Railina <railina30@yandex.ru>
2020-11-11 22:01:56 +0300

    Add validation

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+var d = document;
+
+var name;
+var surname;
+var birthday;
+var mail;
+var password;
+var password2;
+
+function check() {
+    surname = d.getElementById('surname').value;
+    name = d.getElementById('name').value;
+    birthday = d.getElementById('birthday').value;
+    mail = d.getElementById('mail').value;
+    password =  d.getElementById('password').value;
+    password2 = d.getElementById('password2').value;
+
+    var re = new RegExp("[A-Za-zА-Яа-яЁё]{3,}");
+
+    if (!re.test(String(name))) {
+        alert('Некорректное имя');      
+        return;  
+    }
+
+    re = new RegExp("[A-Za-zА-Яа-яЁё]{3,}");
+    if (!re.test(String(surname))) {
+        alert('Некорректная фамилия');       
+        return;  
+    }
+
+    re = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
+    if (!re.test(String(birthday))) {
+        alert('Некорректная дата рождения');
+        return;        
+    }
+
+    if (password2 != password)
+    {
+        alert('Пароли не совпадают!');
+        return;   
+    }
+
+    re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(mail).toLowerCase())) {
+        alert('Некорректная почта');     
+        return;    
+    }
+    
+}
+
+
 
 window.addEventListener("scroll", function () {
 
@@ -13,6 +64,7 @@ window.addEventListener("scroll", function () {
     if (y >= contentHeight) {
         //загружаем новое содержимое в элемент
         block.innerHTML = block.innerHTML + "<div>На реальном сайте получаем новые данные, используя AJAX.";
-        
+
     }
 });
+
