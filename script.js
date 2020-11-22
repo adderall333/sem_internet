4a77def7c9a7329fd90943f0aedce0b6588e1fb6
Railina <railina30@yandex.ru>
2020-11-22 15:34:10 +0300

    Исправила book

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ var mail;
 var password;
 var password2;
 
+function showHide(element_id) {
+    //Если элемент с id-шником element_id существует
+    if (document.getElementById(element_id)) { 
+        //Записываем ссылку на элемент в переменную obj
+        var obj = document.getElementById(element_id); 
+        //Если css-свойство display не block, то: 
+        if (obj.style.display != "block") { 
+            obj.style.display = "block"; //Показываем элемент
+        }
+        else obj.style.display = "none"; //Скрываем элемент
+    }
+    //Если элемент с id-шником element_id не найден, то выводим сообщение
+    else alert("Элемент с id: " + element_id + " не найден!"); 
+}   
+
 function check() {
     surname = d.getElementById('surname').value;
     name = d.getElementById('name').value;
